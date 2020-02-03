71b2de592f7d6bf31bfa2dbfaf1f30faba07cd8a
Alastair Bowie <ally_bowie@hotmail.com>
2020-02-03 21:50:50 +0000

    Filter checkbox handle change now sets new state

--- hotel-front-end/src/filter/filter.js
+++ hotel-front-end/src/filter/filter.js
@@ -3,24 +3,37 @@ import React, { useState } from "react";
 export const Filter = () => {
   const [filter, setFilter] = useState([]);
 
+  const handleChange = event => {
+    const index = filter.indexOf(event.target.value);
+    let newFilters = filter;
+    index > -1
+      ? newFilters.splice(index, 1)
+      : newFilters.push(event.target.value);
+    setFilter(newFilters);
+  };
+
   return (
     <div>
       <form>
         <label>
           Car Park
-          <input type="checkbox" value="car park"></input>
+          <input
+            type="checkbox"
+            value="car park"
+            onChange={handleChange}
+          ></input>
         </label>
         <label>
           Gym
-          <input type="checkbox" value="gym"></input>
+          <input type="checkbox" value="gym" onChange={handleChange}></input>
         </label>
         <label>
           Pool
-          <input type="checkbox" value="pool"></input>
+          <input type="checkbox" value="pool" onChange={handleChange}></input>
         </label>
         <label>
           Pets
-          <input type="checkbox" value="pets"></input>
+          <input type="checkbox" value="pets" onChange={handleChange}></input>
         </label>
       </form>
       <button>Apply Filters:</button>
