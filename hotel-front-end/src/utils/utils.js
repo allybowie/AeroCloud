8e06f4c8c0dbc3e1d89c5517a39e120f4a677e3a
Alastair Bowie <ally_bowie@hotmail.com>
2020-02-03 20:35:59 +0000

    create getHotels function using TDD; returns filtered hotels or all hotels if no filters are passed

--- hotel-front-end/src/utils/utils.js
+++ hotel-front-end/src/utils/utils.js
@@ -1,5 +1,17 @@
 const { testData } = require("./testdata");
 
-const getHotels = () => {};
+const getHotels = (filters = []) => {
+  let hotels = [];
+
+  filters !== []
+    ? testData.forEach(element => {
+        if (filters.every(i => element.facilities.includes(i))) {
+          hotels.push(element);
+        }
+      })
+    : (hotels = testData);
+
+  return hotels;
+};
 
 module.exports = { getHotels };
--- hotel-front-end/src/utils/utils.spec.js
+++ hotel-front-end/src/utils/utils.spec.js
@@ -1,8 +1,23 @@
 const { expect } = require("chai");
 const { getHotels } = require("./utils");
 
-describe("Get Hotels Function: Will also filter results based off selected facilities", () => {
+describe("Get Hotels Function: Will filter results based off selected facilities, or return all hotels if no filters are selected", () => {
   it("returns an array", () => {
     expect(getHotels()).to.be.an("array");
+  });
+  it("returns the complete array of hotels when no arguments are passed", () => {
+    expect(getHotels()).to.have.length(7);
+  });
+  it("returns an array of the correct length when gym is passed in the filter", () => {
+    expect(getHotels(["gym"])).to.have.length(2);
+  });
+  it("returns an array of the correct length when car park is passed in the filter", () => {
+    expect(getHotels(["car park"])).to.have.length(5);
+  });
+  it("returns an array of the correct length when 2 filters are passed", () => {
+    expect(getHotels(["gym", "car park"])).to.have.length(1);
+  });
+  it("returns an array of the correct length when 2 filters are passed", () => {
+    expect(getHotels(["pool", "car park"])).to.have.length(3);
   });
 });
