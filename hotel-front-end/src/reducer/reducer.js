6a10403d240c49c3fc17c67fc6536bbd9d94f93f
Alastair Bowie <ally_bowie@hotmail.com>
2020-02-03 21:56:04 +0000

    set up Redux store

---
+++ hotel-front-end/src/reducer/reducer.js
@@ -0,0 +1,10 @@
+const { testData } = require("../utils/testdata");
+const { getHotels, sortHotels } = require("../utils/utils");
+
+const initialState = {
+  hotels: testData
+};
+
+function reducer(state = initialState, action) {}
+
+export default reducer;
